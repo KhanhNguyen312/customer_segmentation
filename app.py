43d9e820349e215e92e4a99d30410efb5b1e84d0
KhanhNguyen312 <90134286+KhanhNguyen312@users.noreply.github.com>
2024-03-30 06:25:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
